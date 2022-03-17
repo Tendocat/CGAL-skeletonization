3191ef18e4fb7ac134100f865a7ba9530bb054dd
DUMOULIN ANTOINE <antoine.dumoulin2@etu.unistra.fr>
2022-03-17 21:26:59 +0100

    add some init files

---
+++ include/CMakeFiles/sqcheck.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sqcheck.dir/src/main.cpp.o"
+  "CMakeFiles/sqcheck.dir/src/main.cpp.o.d"
+  "sqcheck"
+  "sqcheck.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sqcheck.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ include/CMakeFiles/sqcheck.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for sqcheck.
+# This may be replaced when dependencies are built.
